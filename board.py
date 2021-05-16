0d1350ebb54ca5118236175d857b9b6a3503b0be
Gamattowi <p.romanczuk31@gmail.com>
2021-05-16 14:57:17 +0200

    change location side text during game

--- board.py
+++ board.py
@@ -26,7 +26,7 @@ class Board:
         # draw title over the box
         title = TITLE_FONT.render('TETRIS', True, TEXT_COLOR)
         surface.blit(title, (self.start_x + self.box_width / 2 - (title.get_width() / 2), self.start_y / 2 - title.get_height() / 2))
-        print(self.start_x + self.box_width / 2)
+
         # draw each brick
         for i in range(len(grid)):
             for j in range(len(grid[i])):
@@ -44,16 +44,16 @@ class Board:
 
         preview_x = self.start_x + self.box_width + 50
         preview_y = self.start_y + self.box_height / 2 - 100
-        surface.blit(text, (preview_x + 2.5 * self.block_size - text.get_width() / 2, preview_y - 80 - self.block_size))
+        surface.blit(text, (preview_x + 4 * self.block_size - text.get_width() / 2, preview_y - 80 - self.block_size))
         format = shape.shape[shape.rotation % len(shape.shape)]
 
         # draw score
         label = SCORE_FONT.render(f'SCORE: {score}', True, TEXT_COLOR)
-        surface.blit(label, (preview_x + 2.5 * self.block_size - label.get_width() / 2, preview_y - 10 - self.block_size))
+        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y - 80 - self.block_size))
 
         # draw max score
         label = SCORE_FONT.render(f'MAX SCORE: {get_max_score() if get_max_score() else 0}', True, TEXT_COLOR)
-        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y - 80 - self.block_size))
+        surface.blit(label, (preview_x + 4 * self.block_size - label.get_width() / 2, preview_y + 170))
 
         # draw timer
         label = SCORE_FONT.render(f'TIMER: {format_timer()}', True, TEXT_COLOR)
@@ -65,11 +65,11 @@ class Board:
 
         # draw combo
         label = SCORE_FONT.render(f'COMBO: {combo}', True, TEXT_COLOR)
-        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y + 100))
+        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y + 170))
 
         # max combo
         label = SCORE_FONT.render(f'MAX COMBO: {max_combo}', True, TEXT_COLOR)
-        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y + 170))
+        surface.blit(label, (self.start_x/2 - label.get_width()/2, preview_y + 100))
 
         for i, row in enumerate(format):
             for j, column in enumerate(row):
